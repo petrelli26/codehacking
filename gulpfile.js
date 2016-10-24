54dd149180fb981ba0c7dad83597bc6dd35dfa2e
Pedro Sousa <ma3i_miliano10@hotmail.com>
2016-10-24 16:02:55 +0100

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,39 @@ require('laravel-elixir-vue-2');
  |
  */
 
-elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+//elixir(mix => {
+//    mix.sass('app.scss')
+//       .webpack('app.js');
+//    .styles([
+//        'libs/blog-post.css',
+//        'libs/bootstrap.css',
+//        'libs/font-awesome.css',
+//        'libs/metisMenu.css',
+//        'libs/sb-admin-2.css',
+//    ], './public/css/libs.css')
+//    .scripts([
+//        'libs/bootstrap.js',
+//        'libs/jquery.js',
+//        'libs/metisMenu.js',
+//        'libs/sb-admin-2.js',
+//        'libs/scripts.js'
+//    ], './public/js/libs.js')
+//});
+
+elixir(function(mix) {
+    mix.sass("app.scss");
+    mix.styles([
+        'libs/blog-post.css',
+        'libs/bootstrap.css',
+        'libs/font-awesome.css',
+        'libs/metisMenu.css',
+        'libs/sb-admin-2.css',
+    ], './public/css/libs.css');
+    mix.scripts([
+        'libs/jquery.js',
+        'libs/bootstrap.js',
+        'libs/metisMenu.js',
+        'libs/sb-admin-2.js',
+        'libs/scripts.js'
+    ], './public/js/libs.js');
 });
